a610bbfd9d3e15c76bc8dc291befa8b4420b2480
toneb <tone.bone@protonmail.com>
2023-09-14 15:29:35 +0200

    Create GitExtUtils.Forms project, and update all GitExtUtils references to use new project.

---
+++ GitExtUtils.Forms/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyDescription("GitExtensions common")]
+
+[assembly: InternalsVisibleTo("GitExtUtils.Tests")]
+[assembly: InternalsVisibleTo("GitUI.Tests")]
+[assembly: InternalsVisibleTo("GitExtensions")]
+[assembly: InternalsVisibleTo("BugReporter")]
+[assembly: InternalsVisibleTo("CommonTestUtils")]
+[assembly: InternalsVisibleTo("TranslationApp")]
