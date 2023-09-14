f8eba1f73645c04447353c55b64fa5474be7e0d1
toneb <tone.bone@protonmail.com>
2023-09-14 17:21:13 +0200

    Create GitUI.Forms project, and replace all references to GitUI with new project.

--- GitUI.Forms/Properties/AssemblyInfo.cs
+++ GitUI.Forms/Properties/AssemblyInfo.cs
@@ -3,7 +3,6 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyDescription("GitExtensions UI")]
 
-[assembly: InternalsVisibleTo("GitUI")]
 [assembly: InternalsVisibleTo("GitUI.Tests")]
 [assembly: InternalsVisibleTo("UI.IntegrationTests")]
 [assembly: InternalsVisibleTo("TranslationApp")]
